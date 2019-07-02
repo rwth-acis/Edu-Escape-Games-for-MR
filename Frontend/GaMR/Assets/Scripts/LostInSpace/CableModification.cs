14609ee07621d716f266148a98b650eff7c0f112
Daniel Sous <daniel-11@gmx.de>
2019-07-02 14:32:47 +0200

    Add game logic and comments for the electric circuit quest

--- Frontend/GaMR/Assets/Scripts/LostInSpace/CableModification.cs
+++ Frontend/GaMR/Assets/Scripts/LostInSpace/CableModification.cs
@@ -3,41 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 
-public class CableModification : MonoBehaviour, IFocusable, IInputHandler, IInputClickHandler {
+public class CableModification : MonoBehaviour, IFocusable, IInputClickHandler {
+
+    private Renderer rend;
+    private Shader standard;
+    private Shader focused;
     
 
     // Use this for initialization
-    void Start () {
-        Debug.Log("Started script");
-	}
+    void Awake () {
+        rend = GetComponentInChildren<Renderer>();
+        standard = Shader.Find("Standard");
+        focused = Shader.Find("Diffuse");
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
+    /**
+     * Is called when the cursor points onto this tile. The shader is changed
+     * to give a visual feedback to the user.
+     */
     public void OnFocusEnter()
     {
         Debug.Log("Entered Focus");
+        rend.material.shader = focused;
     }
 
+    /**
+     * Is called when the cursor does not point onto this tile anymore. The shader
+     * is changed back to the original one.
+     */
     public void OnFocusExit()
     {
         Debug.Log("Left Focus");
+        rend.material.shader = standard;
     }
 
-    public void OnInputDown(InputEventData eventData)
-    {
-        Debug.Log("Input is down");
-    }
-
-    public void OnInputUp(InputEventData eventData)
-    {
-        Debug.Log("Input is up");
-    }
-
+    /**
+     * Is called when this tile is clicked. The tile then turns clockwise by 90 degrees.
+     */
     public void OnInputClicked(InputClickedEventData eventData)
     {
         Debug.Log("Input was clicked");
+        this.transform.Rotate(0, 0, 90, Space.Self);
     }
 }
