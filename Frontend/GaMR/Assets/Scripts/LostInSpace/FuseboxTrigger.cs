17e21f7961488a12dfb4c01b7dd640a6755c719f
Daniel Sous <daniel-11@gmx.de>
2019-07-04 15:14:56 +0200

    Trigger when fuse enters fusebox

---
+++ Frontend/GaMR/Assets/Scripts/LostInSpace/FuseboxTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuseboxTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider collider) {
+        string objectTag = collider.gameObject.tag;
+
+        Debug.Log("Was trigger by " + objectTag);
+
+        switch (objectTag) {
+            case "Fuse10":
+                Debug.Log("Fuse 10");
+                break;
+            case "Fuse15":
+                Debug.Log("Fuse 15");
+                break;
+            case "Fuse20":
+                Debug.Log("Fuse 20");
+                break;
+            case "FuseBroken":
+                Debug.Log("Fuse broken");
+                break;
+            default:
+                Debug.Log("You can't put that here");
+                break;
+        }
+    }
+}
