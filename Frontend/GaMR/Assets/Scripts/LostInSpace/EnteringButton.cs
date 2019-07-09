be8a603b70d69b2a015455f442c24d43262c7590
Daniel Sous <daniel-11@gmx.de>
2019-07-09 12:46:57 +0200

    Some strange behavior

---
+++ Frontend/GaMR/Assets/Scripts/LostInSpace/EnteringButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EnteringButton : MonoBehaviour {
+
+    private Button button;
+
+	// Use this for initialization
+	void Start () {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+	}
+	
+	
+	void OnClick () {
+        SceneManager.LoadScene("Lost_in_Space");
+	}
+}
